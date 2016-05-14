f51aebb8076455fe06466c6da4affedc14e4c299
jkhjkhkj <jkgkjuk>
2016-05-14 23:28:36 +0100

    Initial Commit

---
+++ 4 hour stream/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScript : MonoBehaviour {
+
+    public Transform squ;
+    private Vector3 objectpos;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        objectpos = squ.transform.position;
+
+        transform.position = new Vector3(objectpos.x, objectpos.y, transform.position.z);
+	}
+}
---
+++ 4 hour stream/Assets/Scripts/HingeScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HingeScript : MonoBehaviour {
+
+    HingeJoint2D hinge;
+    JointMotor2D motor;
+
+    void Start()
+    {
+        hinge = GetComponent<HingeJoint2D>();
+        motor = hinge.motor;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            motor.motorSpeed = 150;
+            hinge.motor = motor;
+            hinge.useMotor = true;
+        }
+    }
+}
